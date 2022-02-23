b1d2f867f03b742a8b4ffaceab36334e73746be7
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-02-23 22:22:58 +0530

    JavaScript add for hamburger menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav_btn = document.querySelector(".menu-btn");
+const right = document.querySelector(".right");
+
+
+nav_btn.addEventListener("click", () => {
+
+right.classList.toggle("slide-in");
+
+})
